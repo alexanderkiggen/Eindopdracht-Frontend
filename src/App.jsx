1c93f628fc012950737434771aa454c2a7be5c03
alexanderkiggen <alexander.kiggen@gmail.com>
2025-10-20 10:38:15 +0200

    feat: authenticatie voor inloggen en registreren en vergrendeling van favorieten pagina

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,26 @@ import './App.css';
 
 import Home from './pages/Home/Home';
 import Favorieten from './pages/Favorieten/Favorieten';
-import Keuzehulp from "./pages/Keuzehulp/Keuzehulp";
-import Ontdekken from "./pages/Ontdekken/Ontdekken";
-import Informatie from "./pages/Informatie/Informatie";
-import Zoeken from "./pages/Zoeken/Zoeken";
-import NotFound from "./pages/NotFound/NotFound";
-import LoginComponent from './components/Login/Login';
+import Keuzehulp from './pages/Keuzehulp/Keuzehulp';
+import Ontdekken from './pages/Ontdekken/Ontdekken';
+import Informatie from './pages/Informatie/Informatie';
+import Zoeken from './pages/Zoeken/Zoeken';
+import NotFound from './pages/NotFound/NotFound';
 
-import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
+import Login from './components/Login/Login';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
-import DiscordLogo from "./assets/Socials/discord.png";
-import FacebookLogo from "./assets/Socials/facebook.png";
-import InstagramLogo from "./assets/Socials/instagram.png";
-import TwitchLogo from "./assets/Socials/twitch.png";
-import XLogo from "./assets/Socials/x.png";
-import YoutubeLogo from "./assets/Socials/youtube.png";
+import DiscordLogo from './assets/Socials/discord.png';
+import FacebookLogo from './assets/Socials/facebook.png';
+import InstagramLogo from './assets/Socials/instagram.png';
+import TwitchLogo from './assets/Socials/twitch.png';
+import XLogo from './assets/Socials/x.png';
+import YoutubeLogo from './assets/Socials/youtube.png';
 
-// ScrollToTop
+import { getAuth, clearAuth } from './utils/authentication.js';
+
+// Naar boven scrollen bij het openen van een nieuwe pagina
 function ScrollToTop() {
     const { pathname, hash } = useLocation();
     useEffect(() => { if (!hash) window.scrollTo(0, 0); }, [pathname, hash]);
@@ -30,10 +32,21 @@ function ScrollToTop() {
 }
 
 function App() {
+    const [auth, setAuth] = useState(getAuth());
     const [isLoginOpen, setIsLoginOpen] = useState(false);
 
     const openLogin = () => setIsLoginOpen(true);
     const closeLogin = () => setIsLoginOpen(false);
+
+    const handleLoginSuccess = () => {
+        setAuth(getAuth());
+        closeLogin();
+    };
+
+    const handleLogout = () => {
+        clearAuth();
+        setAuth(getAuth());
+    };
 
     useEffect(() => {
         document.body.style.overflow = isLoginOpen ? 'hidden' : '';
@@ -41,28 +54,30 @@ function App() {
     }, [isLoginOpen]);
 
     const pageLinks = [
-        { to: "/ontdekken", children: "Ontdekken" },
-        { to: "/keuzehulp", children: "Keuzehulp" },
-        { to: "/favorieten", children: "Favorieten" },
-        { to: "/", children: "Home" },
+        { to: '/ontdekken', children: 'Ontdekken' },
+        { to: '/keuzehulp', children: 'Keuzehulp' },
+        { to: '/favorieten', children: 'Favorieten' },
+        { to: '/', children: 'Home' },
     ];
 
     const genreLinks = [
-        { to: "#", children: "Action" },
-        { to: "#", children: "Adventure" },
-        { to: "#", children: "Role-Playing Game" },
-        { to: "#", children: "Shooter" },
-        { to: "#", children: "Sports" },
+        { to: '#', children: 'Action' },
+        { to: '#', children: 'Adventure' },
+        { to: '#', children: 'Role-Playing Game' },
+        { to: '#', children: 'Shooter' },
+        { to: '#', children: 'Sports' },
     ];
 
     const socialLinks = [
-        { to: "https://discord.com/", icon: DiscordLogo, alt: "Discord", label: "Discord" },
-        { to: "https://x.com/", icon: XLogo, alt: "X", label: "X (Twitter)" },
-        { to: "https://www.instagram.com/", icon: InstagramLogo, alt: "Instagram" },
-        { to: "https://www.youtube.com/", icon: YoutubeLogo, alt: "YouTube" },
-        { to: "https://www.facebook.com/", icon: FacebookLogo, alt: "Facebook" },
-        { to: "https://www.twitch.tv/", icon: TwitchLogo, alt: "Twitch" },
+        { to: 'https://discord.com/', icon: DiscordLogo, alt: 'Discord', label: 'Discord' },
+        { to: 'https://x.com/', icon: XLogo, alt: 'X', label: 'X (Twitter)' },
+        { to: 'https://www.instagram.com/', icon: InstagramLogo, alt: 'Instagram' },
+        { to: 'https://www.youtube.com/', icon: YoutubeLogo, alt: 'YouTube' },
+        { to: 'https://www.facebook.com/', icon: FacebookLogo, alt: 'Facebook' },
+        { to: 'https://www.twitch.tv/', icon: TwitchLogo, alt: 'Twitch' },
     ];
+
+    const isLoggedIn = !!auth.token;
 
     return (
         <Router>
@@ -73,8 +88,10 @@ function App() {
                     NavigationLinkOneChildren="Ontdekken"
                     NavigationLinkTwoTo="/keuzehulp"
                     NavigationLinkTwoChildren="Keuzehulp"
-                    ButtonPrimaryChildren="Inloggen"
+                    ButtonPrimaryChildren={isLoggedIn ? 'Uitloggen' : 'Inloggen'}
                     onLoginClick={openLogin}
+                    onLogoutClick={handleLogout}
+                    isLoggedIn={isLoggedIn}
                     ButtonSecondaryTo="/favorieten"
                     ButtonSecondaryChildren="Favorieten"
                 />
@@ -85,13 +102,13 @@ function App() {
                         <Route path="/ontdekken" element={<Ontdekken />} />
                         <Route path="/informatie/:game_slug" element={<Informatie />} />
                         <Route path="/keuzehulp" element={<Keuzehulp />} />
-                        <Route path="/favorieten" element={<Favorieten />} />
+                        <Route path="/favorieten" element={<Favorieten user={auth.user} />} />
                         <Route path="/zoeken" element={<Zoeken />} />
                         <Route path="*" element={<NotFound />} />
                     </Routes>
                 </div>
 
-                {isLoginOpen && <LoginComponent onClose={closeLogin} />}
+                {isLoginOpen && <Login onClose={closeLogin} onSuccess={handleLoginSuccess} />}
 
                 <Footer pageLinks={pageLinks} genreLinks={genreLinks} socialLinks={socialLinks} />
             </div>
